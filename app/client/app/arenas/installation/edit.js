dc10f657302dc654bc911c71c852f2679b155f2e
Lachlan Douglas <lachlan@engines.org>
2021-07-19 18:16:51 +1000

    Handle empty form config.

--- app/client/app/arenas/installation/edit.js
+++ app/client/app/arenas/installation/edit.js
@@ -4,13 +4,23 @@ app.arenas.installation.edit = (route) => (a,x) => [
       `/api/installations/@${route.params.arenaIdentifier}::${route.params.blueprintIdentifier}`,
       `/api/blueprints/@${route.params.blueprintIdentifier}/form`,
     ],
-    success: ([installation, form]) => app.formDSL.builder.form({
+    success: ([installation, form]) =>
+    ax.is.object(form.components) && form.components.length ?
+    app.formDSL.builder.form({
       components: form.components,
     }, installation.input, {
       action: `/api/installations/@${route.params.arenaIdentifier}::${route.params.blueprintIdentifier}/input`,
       scope: 'input',
       route: route,
-    }),
+    }) :
+    [
+      a.p(app.placeholder('Nothing to configure.')),
+      app.button({
+        label: app.icon('fas fa-check', 'Done'),
+        onclick: () => route.open('..'),
+        class: 'btn btn-primary',
+      }),
+    ],
   }),
 
 ]
