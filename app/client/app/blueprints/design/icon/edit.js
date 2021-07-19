834e4c40e34cdb8fcc80b5acfa70ebe6471a96a3
Lachlan Douglas <lachlan@engines.org>
2021-07-20 07:20:45 +1000

    Use iframes to cache-bust icons

--- app/client/app/blueprints/design/icon/edit.js
+++ app/client/app/blueprints/design/icon/edit.js
@@ -3,58 +3,69 @@ app.blueprints.design.icon.edit = (route) => (a, x) => [
   app.float({
     left: [
       a({
-        id: 'iconFrame',
         $nodes: (el) => [
           a({
             $tag: 'iframe',
+            id: 'iconFrameRaw',
             src: `/api/blueprints/@${route.params.blueprintIdentifier}/icon/raw`,
             height: 52,
             width: 52,
-            class: 'border border-1',
-          }),
-          a({
-            $tag: 'iframe',
-            src: `/api/blueprints/@${route.params.blueprintIdentifier}/icon/bordered`,
-            height: 0,
-            width: 0,
             style: {border: 'none'},
           }),
           a({
             $tag: 'iframe',
+            id: 'iconFrameThumbnail',
             src: `/api/blueprints/@${route.params.blueprintIdentifier}/icon/thumbnail`,
             height: 0,
             width: 0,
             style: {border: 'none'},
           }),
+          a({
+            $tag: 'iframe',
+            id: 'iconFrameBordered',
+            src: `/api/blueprints/@${route.params.blueprintIdentifier}/icon/bordered`,
+            height: 0,
+            width: 0,
+            style: {border: 'none'},
+          }),
+          a.div([
+            x.filepond({
+              inputTag: {
+                name: 'icon',
+              },
+              filepondTag: {
+                id: 'iconUploader',
+              },
+              filepond: {
+                credits: false,
+                labelIdle: 'Drop icon file, or <span class="filepond--label-action">browse</span>.',
+                labelTapToUndo: '',
+                allowRevert: false,
+                server: {
+                  process: {
+                    url: `/api/blueprints/@${route.params.blueprintIdentifier}/icon`,
+                    method: 'PUT',
+                    onload: () => setTimeout(() => {
+                      let filepond = iconUploader.$filepond
+                      if (filepond.getFiles().length != 0) {
+                        for (let i = 0; i <= filepond.getFiles().length - 1; i++) {
+                          filepond.removeFile(filepond.getFiles()[0].id)
+                        }
+                      }
+                      iconFrameRaw.contentWindow.location.reload(true)
+                      iconFrameThumbnail.contentWindow.location.reload(true)
+                      iconFrameBordered.contentWindow.location.reload(true)
+                    }, 1500)
+                  },
+                }
+              },
+            })
+          ], {
+            style: 'width: 300px;',
+          }),
         ],
         class: 'p-1',
       }),
-
-      a.div([
-        x.filepond({
-          inputTag: {
-            name: 'icon',
-          },
-          filepond: {
-            credits: false,
-            labelIdle: 'Drop icon file, or <span class="filepond--label-action">browse</span>.',
-            labelTapToUndo: '',
-            allowRevert: false,
-            server: {
-              process: {
-                url: `/api/blueprints/@${route.params.blueprintIdentifier}/icon`,
-                method: 'PUT',
-                onload: () => {
-                  setTimeout(iconFrame.$render, 1000)
-                }
-              },
-            }
-          },
-        })
-      ], {
-        style: 'width: 300px;',
-      }),
-
     ],
     right: [
       app.button({
