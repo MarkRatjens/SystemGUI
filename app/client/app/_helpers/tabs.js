be00ee5d96a225084659e865aa01ef7d609c5d37
Lachlan Douglas <lachlan@engines.org>
2021-07-27 19:53:05 +1000

    Tabs check for invalid inputs

--- app/client/app/_helpers/tabs.js
+++ app/client/app/_helpers/tabs.js
@@ -1,27 +1,41 @@
 app.tabs = (options) => (a,x) => a['div.app-tabs']([
   a["ul.nav.nav-tabs"](
     (options.tabs || []).map((tab, i) => a["li.nav-item"]([
-      a.a(tab[0], {
+      a.a(tab.label, {
+        tabindex: 0,
+        ...options.navTag,
         class: `nav-link${i == 0 ? ' active' : ''}`,
         $on: {
-          click: (e,el) => {
+          'click: open a tab': (e,el) => {
             e.preventDefault
             el.$('^.app-tabs').$open(i)
           },
         },
-      })
+      }),
     ])),
   ),
-  (options.tabs || []).map((tab, i) => a["div.app-tab-body"]([
-    tab[1]
+  (options.tabs || []).map((tab, i) => a["app-tabs-tab-body"]([
+    tab.body
   ], {
-    class: i == 0 ? '' : 'd-none',
+    ...options.tabBodyTag,
+    class: `${options.tabBodyTag.class} ${i == 0 ? '' : 'd-none'}`,
   })),
 ], {
   $open: (el) => (i) => {
+    if (el.$$('a.nav-link').$$[i].classList.contains('active')) return
+    let body = el.$('app-tabs-tab-body:not(.d-none)')
+    let valid = body.$$('input, textarea, select').checkValidity().$$.every(
+      validity => validity == true
+    )
+    if (!valid) {
+      let firstInvalid = body.$('input:invalid, textarea:invalid, select:invalid')
+      firstInvalid && firstInvalid.reportValidity()
+      return
+    }
     el.$('a.nav-link.active').classList.remove('active')
     el.$$('a.nav-link').$$[i].classList.add('active')
-    el.$$('div.app-tab-body').classList.add('d-none')
-    el.$$('div.app-tab-body').$$[i].classList.remove('d-none')
-  }
+    el.$$('app-tabs-tab-body').classList.add('d-none')
+    el.$$('app-tabs-tab-body').$$[i].classList.remove('d-none')
+  },
+  ...options.tabsTag,
 })
