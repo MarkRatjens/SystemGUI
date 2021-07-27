136d91b550793152aebbf0f3cfdfb12c8d8c82ed
Lachlan Douglas <lachlan@engines.org>
2021-07-27 19:46:48 +1000

    Better branch checkout form

--- app/client/app/blueprints/design/branch.js
+++ app/client/app/blueprints/design/branch.js
@@ -1,5 +1,5 @@
 app.blueprints.design.branch = (route, blueprint) => (a, x) => a.div([
-  a.h3(`Branch blueprint`),
+  a.h3(`Branch`),
   app.fetch({
     url: `/api/blueprints/@${route.params.blueprintIdentifier}`,
     success: blueprint => [
@@ -8,12 +8,36 @@ app.blueprints.design.branch = (route, blueprint) => (a, x) => a.div([
         method: "POST",
         form: (f) => [
           f.field({
+            key: 'existing',
+            label: 'Checkout',
+            as: 'radios',
+            value: 'existing',
+            selections: {
+              'existing': 'Existing branch',
+              'new': 'New branch',
+            },
+          }),
+          f.field({
             key: 'branch',
-            as: 'selectinput',
-            placeholder: 'Select or enter a branch',
+            label: false,
+            as: 'select',
             required: true,
             value: blueprint.publication.branch,
             selections: blueprint.publication.branches,
+            dependent: {
+              key: 'existing',
+              value: 'existing',
+            },
+          }),
+          f.field({
+            key: 'branch',
+            label: false,
+            placeholder: 'New branch',
+            required: true,
+            dependent: {
+              key: 'existing',
+              value: 'new',
+            },
           }),
           f.buttons({route: route}),
         ],
