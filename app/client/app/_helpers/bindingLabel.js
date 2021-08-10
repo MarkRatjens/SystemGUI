d39e7478ed051fd57f0c4e065ee8d4e5c1fd8115
Lachlan Douglas <lachlan@engines.org>
2021-08-10 10:08:50 +1000

    Expressive binding label

--- app/client/app/_helpers/bindingLabel.js
+++ app/client/app/_helpers/bindingLabel.js
@@ -1,3 +1,5 @@
-app.bindingLabel = (binding) =>
-(binding.identifier && binding.identifier != binding.target_identifier) ?
-`${binding.target_identifier} (${binding.identifier})` : binding.target_identifier
+app.bindingLabel = (binding) => (a,x) => [
+  (binding.identifier && binding.identifier != binding.target_identifier) ?
+  `${binding.target_identifier} (${binding.identifier})` : binding.target_identifier,
+  binding.type ? a.small(` ${binding.type}`) : null
+]
