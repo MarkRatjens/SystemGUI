192577c85cd124d928ae11b9b4d49b98bf863fca
Lachlan Douglas <lachlan@engines.org>
2021-08-11 13:36:20 +1000

    Make menus more reactive

--- app/client/app/blueprints/design/menu.js
+++ app/client/app/blueprints/design/menu.js
@@ -1,49 +1,54 @@
-app.blueprints.design.menu = (route) => (a,x) => {
-
-  let renderMenu = (active) => [
+app.blueprints.design.menu = (route) => (a,x) => a({
+  class: 'activatable',
+  $nodes: [
     app.button({
       label: 'Blueprint',
+      data: {view: 'blueprint'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design`),
-      class: `btn app-btn${active == 'blueprint' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Icon',
+      data: {view: 'icon'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design/icon`),
-      class: `btn app-btn${active == 'icon' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Readme',
+      data: {view: 'readme'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design/readme`),
-      class: `btn app-btn${active == 'readme' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'License',
+      data: {view: 'license'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design/license`),
-      class: `btn app-btn${active == 'license' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Form',
+      data: {view: 'form'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design/form`),
-      class: `btn app-btn${active == 'form' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Files',
+      data: {view: 'files'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/design/files`),
-      class: `btn app-btn${active == 'files' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
-  ]
-
-  return route.mount({
-    routes: {
-      '/icon/?*': (route) => renderMenu('icon'),
-      '/readme/?*': (route) => renderMenu('readme'),
-      '/license/?*': (route) => renderMenu('license'),
-      '/form/?*': (route) => renderMenu('form'),
-      '/files/?*': (route) => renderMenu('files'),
-      '*': (route) => renderMenu('blueprint'),
-    },
-    lazy: false,
-    transition: false,
-  })
-
-}
+  ],
+  $init: (el) => el.$activate(),
+  $update: (el, active) => {
+    el.$$(`button`).classList.remove('active')
+    el.$(`button[data-view="${active}"]`).classList.add('active')
+  },
+  $activate: (el) => () => {
+    let fragment = window.location.pathname.split('/')[4]
+    if (el.$(`button[data-view="${fragment}"]`)) {
+      el.$state = fragment
+    } else {
+      el.$state = 'blueprint'
+    }
+  },
+})
--- app/client/app/blueprints/menu.js
+++ app/client/app/blueprints/menu.js
@@ -1,37 +1,43 @@
-app.blueprints.menu = (route) => (a,x) => {
-
-  let renderMenu = (active) => [
+app.blueprints.menu = (route) => (a,x) => a({
+  class: 'activatable',
+  $nodes: [
     app.button({
       label: 'Readme',
+      data: {view: 'readme'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}`),
-      class: `btn app-btn${active == 'readme' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'License',
+      data: {view: 'license'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/license`),
-      class: `btn app-btn${active == 'license' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Bindings',
+      data: {view: 'bindings'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/bindings`),
-      class: `btn app-btn${active == 'bindings' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
     app.button({
       label: 'Relations',
+      data: {view: 'relations'},
       onclick: () => route.open(`/blueprints/@${route.params.blueprintIdentifier}/relations`),
-      class: `btn app-btn${active == 'relations' ? ' active' : ''}`,
+      class: 'btn app-btn',
     }),
-  ]
-
-  return route.mount({
-    routes: {
-      '/license/?*': (route) => renderMenu('license'),
-      '/bindings/?*': (route) => renderMenu('bindings'),
-      '/relations/?*': (route) => renderMenu('relations'),
-      '*': (route) => renderMenu('readme'),
-    },
-    lazy: false,
-    transition: false,
-  })
-
-}
+  ],
+  $init: (el) => el.$activate(),
+  $update: (el, active) => {
+    el.$$(`button`).classList.remove('active')
+    if (!el.$(`button[data-view="${active}"]`)) {debugger}
+    el.$(`button[data-view="${active}"]`).classList.add('active')
+  },
+  $activate: (el) => () => {
+    let fragment = window.location.pathname.split('/')[3]
+    if (el.$(`button[data-view="${fragment}"]`)) {
+      el.$state = fragment
+    } else {
+      el.$state = 'readme'
+    }
+  },
+})
