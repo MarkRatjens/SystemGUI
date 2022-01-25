7d39cb259731d3af492e1b643ab59b0cbc920ddc
Lachlan Douglas <lachlan@engines.org>
2022-01-25 13:02:59 +1100

    No preload of list of domains

--- app/client/app/dashboard/body.js
+++ app/client/app/dashboard/body.js
@@ -2,11 +2,9 @@ app.dashboard.body = (route) => (a,x) => [
   app.fetch({
     url: [
       '/api/settings',
-      '/api/domains',
     ],
     success: ([settings, domains, providers]) => {
       app.universe.settings = settings
-      app.universe.domains = domains
       return [
         app.universeLabel(settings.about || {}),
         a['div.p-1']([
