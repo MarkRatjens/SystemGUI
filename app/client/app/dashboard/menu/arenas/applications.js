ade83db56936c18951c8a6fe63f0f5fc68e1f4b1
Lachlan Douglas <lachlan@engines.org>
2022-01-25 13:20:44 +1100

    Remove unused 'application' model from arena menu

--- app/client/app/dashboard/menu/arenas/applications.js
+++
@@ -1,8 +0,0 @@
-app.dashboard.menu.arenas.applications = (route, arenaIdentifier) => (a,x) => [
-  app.fetch({
-    url: `/api/applications`,
-    query: {arena_identifier: arenaIdentifier},
-    success: applications => applications.sort().map(application =>
-      app.dashboard.menu.arenas.applications.application(route, application)),
-  }),
-]
